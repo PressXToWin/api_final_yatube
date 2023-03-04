521aadb9e058900d790c497277cf7cad93ddcef3
PressXToWin <PressXToWin@users.noreply.github.com>
2023-03-04 22:19:25 +0700

    add get and post requests on post model

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,12 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets 
+
+from posts.models import Post
+from .serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
