d616586933e9675624346e267037c906454c334d
PressXToWin <PressXToWin@users.noreply.github.com>
2023-03-05 21:35:15 +0700

    fully implemented post, comment and group models

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from posts.models import Post, Group, Comment
+from rest_framework.pagination import LimitOffsetPagination
+
 from .serializers import PostSerializer, CommentSerializer, GroupSerializer
 from .permissions import IsAuthorOrReadOnly
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -22,7 +27,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
